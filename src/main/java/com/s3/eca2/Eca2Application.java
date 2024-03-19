05e6a99fd0f8be21761c93e27ede125d538e8872
KMCgadi <kmc@gadi.co.kr>
2024-03-19 14:40:04 +0900

    parquet 임시 저장경로 상대경로로 변경 (클래스패스 내 경로로)

--- src/main/java/com/s3/eca2/Eca2Application.java
+++ src/main/java/com/s3/eca2/Eca2Application.java
@@ -8,6 +8,7 @@ public class Eca2Application {
 
 	public static void main(String[] args) {
 		System.setProperty("hadoop.home.dir", "D:\\util\\hadoop-3.3.6"); // 리눅스 배포시 하둡이 설치된 실제 디렉토리 경로로 변경해야함
+//		System.setProperty("hadoop.home.dir", "/path/to/hadoop-3.3.6");
 		SpringApplication.run(Eca2Application.class, args);
 	}
 
