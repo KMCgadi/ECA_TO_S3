a14821e5a6b11d4f85e5c555b654141200b587a5
lsy2345 <lsy@gadi.co.kr>
2024-03-25 14:55:48 +0900

    티켓채널 parquet 컨버터 2번쨰 수정(필요없는 메소드 삭제 및 필드명 테이블 스키마에 맞게 변경)

--- src/main/java/com/s3/eca2/api/ticketChannel/ChannelToParquetConverter.java
+++ src/main/java/com/s3/eca2/api/ticketChannel/ChannelToParquetConverter.java
@@ -24,7 +24,6 @@ import static org.apache.parquet.schema.Type.Repetition.REQUIRED;
 
 @Component
 public class ChannelToParquetConverter {
-
     private static final MessageType SCHEMA = Types.buildMessage()
             .addField(Types.primitive(INT64, REQUIRED).named("TICKET_CHANNEL_EID"))
             .addField(Types.optional(PrimitiveType.PrimitiveTypeName.BINARY).as(OriginalType.UTF8).named("ENTITY_STATUS"))
@@ -39,9 +38,7 @@ public class ChannelToParquetConverter {
             .addField(Types.optional(INT64).named("MOD_USER_ENTITY_ID"))
             .addField(Types.optional(INT64).named("REG_USER_ENTITY_ID"))
             .named("TicketChannel");
-
     private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
     public void writeTicketChannelToParquet(List<Channel> channels, String fileOutputPath) throws IOException {
         try (ParquetWriter<Channel> writer = new TicketChannelParquetWriter(new Path(fileOutputPath),SCHEMA)){
             for (Channel channel : channels){
@@ -54,13 +51,10 @@ public class ChannelToParquetConverter {
             super(file, new TicketChannelWriteSupport(schema));
         }
     }
-
     private static class TicketChannelWriteSupport extends WriteSupport<Channel> {
         private MessageType schema;
         private RecordConsumer recordConsumer;
-
         public TicketChannelWriteSupport(MessageType schema) { this.schema = schema;}
-
         @Override
         public WriteContext init(org.apache.hadoop.conf.Configuration configuration) {
             return new WriteContext(schema, new java.util.HashMap<>());
@@ -69,24 +63,22 @@ public class ChannelToParquetConverter {
         public void prepareForWrite(RecordConsumer recordConsumer) {
             this.recordConsumer = recordConsumer;
         }
-
         @Override
         public void write(Channel channel){
             recordConsumer.startMessage();
-            writeNullableLongField("ticketChannelEid",0, channel.getTicketChannelEid());
-            writeStringField("entityStatus",1, channel.getEntityStatus());
-            writeStringField("modDate",2, dateToString(channel.getModDate()));
-            writeStringField("regDate",3, dateToString(channel.getRegDate()));
-            writeStringField("contactCode",4, channel.getContactCode());
-            writeStringField("endDate",5, channel.getEndDate());
-            writeStringField("startDate",6, channel.getStartDate());
-            writeNullableLongField("ticketEid",7, channel.getTicketEid());
-            writeStringField("typeCode",8, channel.getTypeCode());
-            writeStringField("processDate",9, channel.getProcessDate());
-            writeNullableLongField("modUserEntityId",10, channel.getModUserEntityId());
-            writeNullableLongField("regUserEntityId",11, channel.getRegUserEntityId());
+            writeNullableLongField("TICKET_CHANNEL_EID",0, channel.getTicketChannelEid());
+            writeStringField("ENTITY_STATUS",1, channel.getEntityStatus());
+            writeStringField("MOD_DATE",2, dateToString(channel.getModDate()));
+            writeStringField("REG_DATE",3, dateToString(channel.getRegDate()));
+            writeStringField("CONTACT_CD",4, channel.getContactCode());
+            writeStringField("END_DATE",5, channel.getEndDate());
+            writeStringField("START_DATE",6, channel.getStartDate());
+            writeNullableLongField("TICKET_EID",7, channel.getTicketEid());
+            writeStringField("TYPE_CD",8, channel.getTypeCode());
+            writeStringField("PROCESS_DATE",9, channel.getProcessDate());
+            writeNullableLongField("MOD_USER_ENTITY_ID",10, channel.getModUserEntityId());
+            writeNullableLongField("REG_USER_ENTITY_ID",11, channel.getRegUserEntityId());
             recordConsumer.endMessage();
-
         }
         private void writeStringField(String fieldName, int fieldIndex, String value) {
             if (value != null) {
@@ -95,20 +87,9 @@ public class ChannelToParquetConverter {
                 recordConsumer.endField(fieldName, fieldIndex);
             }
         }
-
         private String dateToString(Date date) {
             return date != null ? dateFormat.format(date) : null;
         }
-
-        private void writeDateField(String fieldName, int fieldIndex, Date date) {
-            if (date != null) {
-                long timeInMillis = date.getTime(); // 날짜를 밀리초로 변환
-                recordConsumer.startField(fieldName, fieldIndex);
-                recordConsumer.addLong(timeInMillis); // TIMESTAMP_MILLIS 타입으로 기록
-                recordConsumer.endField(fieldName, fieldIndex);
-            }
-        }
-
         private void writeNullableLongField(String fieldName, int fieldIndex, Long value) {
             if (value != null) {
                 recordConsumer.startField(fieldName, fieldIndex);
